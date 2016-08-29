568f77d769c450330fc7cd22069692bca3c8ae32
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-08-29 10:26:37 +0800

    :bug: make reload possible

--- app/menu.js
+++ app/menu.js
@@ -112,15 +112,6 @@ let template = [{
             accelerator: 'CmdOrCtrl+R',
             click: function(item, focusedWindow) {
                 if (focusedWindow) {
-                    // on reload, start fresh and close any old
-                    // open secondary windows
-                    if (focusedWindow.id === 1) {
-                        BrowserWindow.getAllWindows().forEach(function(win) {
-                            if (win.id > 1) {
-                                win.close()
-                            }
-                        })
-                    }
                     focusedWindow.reload()
                 }
             }
